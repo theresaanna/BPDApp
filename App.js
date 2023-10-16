235d058e2767fd7fd7cf8edb68e1226d92b2ffb5
Theresa Summa <theresasumma@gmail.com>
2023-10-15 17:46:56 -0700

    change base classname to light or dark based on user's scheme

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Appearance, useColorScheme } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function App() {
+  let colorScheme = useColorScheme();
   return (
     <SafeAreaProvider>
-      <View style={styles.container}>
+      <View style={styles.container} className=colorScheme>
         <NavigationContainer>
           <Text>Open up App.js to start working on your app!</Text>
           <StatusBar style="auto" />
