bc47ab21f0ce71fe35f603006ba44ed6a023ae0b
Theresa Summa <theresasumma@gmail.com>
2023-11-20 21:49:41 -0800

    some graphql and apollo scaffolding

--- App.js
+++ App.js
@@ -1,21 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Appearance, useColorScheme } from 'react-native';
+import { StyleSheet, Text, View, Appearance, AppRegistry, useColorScheme } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
 
 export default function App() {
   let colorScheme = useColorScheme();
+
+  const client = new ApolloClient({
+    uri: 'http://localhost:4000/graphql',
+    cache: new InMemoryCache()
+  });
+
   return (
-    <SafeAreaProvider>
-      <View style={styles.container} className={colorScheme}>
-        <NavigationContainer>
-          <StatusBar style="auto" />
-        </NavigationContainer>
-      </View>
-    </SafeAreaProvider>
+    <ApolloProvider client={client}>
+      <SafeAreaProvider>
+        <View style={styles.container} className={colorScheme}>
+          <NavigationContainer>
+            <StatusBar style="auto" />
+          </NavigationContainer>
+          <Header>
+
+          </Header>
+        </View>
+      </SafeAreaProvider>
+    </ApolloProvider>
   );
 }
+
+AppRegistry.registerComponent('BPDApp', () => App);
 
 const styles = StyleSheet.create({
   container: {
